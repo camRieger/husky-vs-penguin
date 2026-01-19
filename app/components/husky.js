51c324f4b7e119a94708e6472b4cc3e85f8be2f4
Tanner Petreman <tanner.petreman@edu.sait.ca>
2026-01-19 15:07:30 -0700

    made husky page
    create the husky page

---
+++ app/components/husky.js
@@ -0,0 +1,45 @@
+export default function Husky() {
+  return
+  <main>
+    <h1>Siberian Husky — Strengths & Weaknesses</h1>
+    <h2>Strengths</h2>
+    <h3>Speed & Agility</h3>
+    <p> -Very fast sprinter and long-distance runner.
+      -Excellent turning speed and footwork.
+      -Can quickly disengage or reposition.</p>
+
+
+    <h3>Bite Weaponry</h3>
+    <p>
+      Strong jaws with sharp canine teeth.
+      Capable of:
+      Deep puncture wounds
+      Tearing flesh with head-shaking
+      More dangerous than most birds or similarly sized animals.
+    </p>
+
+    <h3>Endurance</h3>
+    <p>
+      Bred for long-distance sled pulling.
+      Can fight or flee longer than many animals of similar size.
+    </p>
+
+    <h3>Cold Resistance</h3>
+    <p>
+      Thick double coat reduces fatigue and damage in freezing conditions.
+      Maintains effectiveness where many animals slow down.
+    </p>
+
+    <h3>Pack Instinct</h3>
+    <p>
+      Coordinated movement when with other dogs.
+      Flanking, harassment, and chase behaviors come naturally.
+      High Situational Awareness
+      Strong hearing and smell.
+      Good threat detection and reaction time.
+      Pain Tolerance
+      Working-dog heritage = moderate pain resistance.
+      Less likely to panic from minor injuries.
+    </p>
+  </main>
+}
